46ade2fb5acc8c188007f67860f9a7396661e26d
victor-montmorency <67494565+victor-montmorency@users.noreply.github.com>
2025-01-06 12:39:55 -0300

    UserInfoHeader

---
+++ GitHubFollowers/Custom Views/GFSecondaryTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by victor mont-morency on 06/01/25.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat){
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        setup()
+    }
+    
+    private func setup(){
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
